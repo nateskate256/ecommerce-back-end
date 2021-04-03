2c9bdce9e4dc591693f205360fdda9967bed038f
Nate Shipp <nateskate256@gmail.com>
2021-04-02 19:45:31 -0700

    models setup

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,60 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  const categoryData = await Category.findAll();
+  return res.json(categoryData)
   // be sure to include its associated Products
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  const categoryData = await Category.findByPk(req.params.id)
+  return res.json(categoryData);
   // be sure to include its associated Products
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const categoryData = await Category.create(req.body)
+
+  return res.json(categoryData);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const categoryData = await Category.update(
+    {
+      shirts: req.body.shirts,
+      shorts: req.body.shorts,
+      music: req.body.music,
+      hats: req.body.hats,
+      shoes: req.body.shoes,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+
+  return res.json(categoryData);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+
+  return res.json(categoryData);
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,16 +5,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  const tagData = await Tag.findAll();
+  return res.json(tagData)
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  const tagData = await Tag.findByPk(req.params.id, {
+    include: [{ model: Product}]
+  })
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  const tagData = await Tag.create(req.body)
+  return res.json(tagData)
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +30,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  const tagData = await Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
 });
 
 module.exports = router;
